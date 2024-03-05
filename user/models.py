ccc88f58cfdf6329a94a228acc293a80457902d7
Dosu333 <vladamir1865@gmail.com>
2024-03-05 11:42:42 +0100

    user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from datetime import datetime
+import uuid
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    email = models.EmailField(
+        max_length=255, unique=True, blank=True, null=True)
+    password = models.CharField(max_length=225, null=True)
+    first_name = models.CharField(max_length=225, blank=True, null=True)
+    last_name = models.CharField(max_length=225, blank=True, null=True)
+    phone = models.CharField(max_length=17, blank=True, null=True)
+    country = models.CharField(max_length=225, blank=True, null=True)
+    state = models.CharField(max_length=225, blank=True, null=True)
+    city = models.CharField(max_length=225, blank=True, null=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    last_login = models.DateTimeField(null=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    verified = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+    class Meta:
+        ordering = ('-date_joined',)
+
+    def __str__(self):
+        return self.email
+
+    def save_last_login(self):
+        self.last_login = datetime.now()
+        self.save()

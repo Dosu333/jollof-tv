b1fa953d1cafc38a9f03ccdc4b155aa8b720bf1c
Dosu333 <vladamir1865@gmail.com>
2024-03-05 15:16:40 +0100

    debug

--- user/utils.py
+++ user/utils.py
@@ -3,7 +3,8 @@ from decouple import config
 
 def send_password_reset_email(email_data):
     subject = 'Password Reset'
-    password_rest_url = f'{config('CLIENT_URL')}/password-rest/{email_data['token']}'
+    client_url = config('CLIENT_URL')
+    password_rest_url = f'{client_url}/password-rest/{email_data['token']}'
     message = f'Hello {email_data["firstname"]},\n\n Below is the link to reset your password \n\n {password_rest_url}' 
     send_mail(
         subject,

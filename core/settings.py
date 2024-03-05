7b9e0b9bdabaaa72db1f89119e95b4f10029754f
Dosu333 <vladamir1865@gmail.com>
2024-03-05 19:42:53 +0100

    video upload

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 from decouple import config
 from datetime import timedelta
 import cloudinary
-          
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,7 +150,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- video/models.py
+++ video/models.py
@@ -9,15 +9,15 @@ class VideoFile(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, blank=True, null=True)
     title = models.CharField(max_length=255, blank=True, null=True)
-    video_file = CloudinaryField('video')
+    video_file = CloudinaryField(resource_type='video', blank=True, null=True)
     thumbnail = CloudinaryField('thumbnail', blank=True, null=True)
 
-    def save(self, *args, **kwargs):
-        # Generate a transformation URL to extract the first frame as a thumbnail
-        if self.video_file and not self.thumbnail:
-            thumbnail_url = cloudinary.CloudinaryImage(self.video_file.url).video_thumbnail()
-            self.thumbnail = thumbnail_url
-        return super().save(*args, **kwargs)
+    # # def save(self, *args, **kwargs):
+    # #     # Generate a transformation URL to extract the first frame as a thumbnail
+    # #     if self.video_file and not self.thumbnail:
+    # #         thumbnail_url = cloudinary.CloudinaryImage(self.video_file).video_thumbnail()
+    # #         self.thumbnail = thumbnail_url
+    # #     return super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
